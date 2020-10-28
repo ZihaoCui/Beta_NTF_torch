f11337ee37dbf78c3ea25b5473549d08244ce9dd
ZihaoCui <32896820+ZihaoCui@users.noreply.github.com>
2020-10-28 16:27:55 +0800

    Update beta_ntf_torch1.py

--- beta_ntf_torch1.py
+++ beta_ntf_torch1.py
@@ -2,12 +2,10 @@
 """
 --------------------------------------------
 modified by Zihao Cui 2018
+
+Copyright © 2018 SASPL
+Auteur(s) : ZIHAO CUI
 the origin one is also given
-
---------------------------------------------
-
-Copyright © 2012 Telecom ParisTech, TSI
-Auteur(s) : Liutkus Antoine
 
 --------------------------------------------
 the beta_ntf module is free software: you can redistribute it and/or modify
@@ -245,42 +243,4 @@ def parafac(factors):
     request = request[:-1] + '->' + string.ascii_lowercase[:ndims]
     return torch.einsum(request, (factors[0],factors[1]))
 
-
-
-
-#if __name__ == '__main__':
-#    # Choosing the shape of the data to approximate (tuple of length up to 25)
-#    data_shape = (1000, 400, 10)  # 3-way tensor
-#    sparse = np.array([1, 0, 0])
-#    temporal = np.array([1, 0, 0])
-#    # data_shape = (1000, 400)  # matrix
-#    # data_shape = (50, 5, 10, 6, 7)  # 5-way tensor
-
-#    # Choosing the number of components for testing
-#    n_components = 9
-
-#    # Building the true factors to generate data
-#    factors = [nnrandn((shape, n_components)) for shape in data_shape]
-
-#    # Generating the data through the parafac function
-#    V = parafac(factors)
-
-#    # Create BetaNTF object with Euclidean (beta=2) distance
-#    beta_ntf = BetaNTF(V.shape, n_components=10, beta=1, n_iter=100,
-#                       verbose=True, sparse=sparse, temporal=temporal)
-
-#    # Fit the model
-#    beta_ntf.fit([V])
-
-#    #Print resulting score
-#    print ('Resulting score', beta_ntf.score(V))
-#    print ('Compression ratio : %0.1f%%'%((1.-sum(beta_ntf.data_shape)*
-#                        beta_ntf.n_components
-#                        /float(np.prod(beta_ntf.data_shape)))*100.))
-        
-
-#    #Now illustrate the get model
-#    total_model= parafac(beta_ntf.factors_)
-#    two_components = beta_ntf[...,:2]
-#    print ('Shape of total_model : ',total_model.shape)
 #    print ('Shape of two_components : ',two_components.shape)
